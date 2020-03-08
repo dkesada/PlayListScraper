c2b96f8864f98c38ab4e8361256878dca58c28a4
dkesada <dkesada@gmail.com>
2020-03-08 12:46:34 +0100

    Youtube changed its format. Redoing

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PlayListScraper
-Web scraper for youtube playlists.
+Web scraper for YouTube playlists.
 
-I have many playlists in YouTube of 300+ videos, and keeping record of the videos inside them is a mess. Whenever a video gets deleted by the uploader or by YouTube, it simply says "Deleted video" (same for when it goes private). So, if I wanted to replace that video, I had to remember wich one was it. Needless to say, it's not possible with lengthy playlists.
+I have many YouTube playlists of 300+ videos, and keeping record of the videos inside them is a mess. Whenever a video gets deleted by the uploader or by YouTube, it simply says "Deleted video" (same thing when it goes private). So, if I wanted to replace that video, I had to remember which one was it. Needless to say, it's not possible with lengthy playlists.
 
 In order to avoid this, I used to make screenshots of the playlist and I would keep them in a file for future reference in case a video got deleted.
 
--- playListScraper.py
+++ playListScraper.py
@@ -7,46 +7,31 @@ from bs4 import BeautifulSoup
 from PIL import Image
 from cStringIO import StringIO
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from re import split
-from time import sleep
-from pdfCreator import pdfTable
 
 
 # The playlist url, constant for now
 url = 'https://www.youtube.com/playlist?list=PLPV3XXS84jhAFXrEIlNJ-gMwNqUpnTL6l'
 
-# Fist, I have to load the full playlist with the 'Load more' button. I'll use selenium for that
-driver = webdriver.Firefox()
+# Fist, I have to load the full playlist going to the bottom of the list. I'll use selenium for that
+driver = webdriver.Chrome('./PlayListScraper/driver/chromedriver.exe')
 driver.get(url)
+last, current = -1, 0
+
 try:
-	while(True):
-		wait = WebDriverWait(driver, 1)
-		element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'browse-items-load-more-button')))
-		element.click()
-		sleep(0.5) # To avoid the stale element by clicking the same button twice
-except (NoSuchElementException, TimeoutException):
+	while last < current:
+		soup = BeautifulSoup(driver.page_source, 'html.parser')
+		last = current
+		current = len(soup.find_all('div', {'id':'content'}))
+		driver.execute_script('window.scrollTo(0, document.getElementById("page-manager").scrollHeight);')
+except TimeoutException:
 	pass
 
-html = driver.page_source
 driver.quit()
-soup = BeautifulSoup(html,'html.parser')
+soup = BeautifulSoup(driver.page_source,'html.parser')
 
-# Header
-header = soup.find(id='pl-header')
-imgUrl = header.find('img')['src']
-img = Image.open(StringIO(urlopen(imgUrl).read())) # Playlist thumbnail
-title = header.find('h1').get_text()
-title = title[5:len(title)-3] # Cleaning the title
-data = header.find('ul').find_all('li')
-for i in range(3):
-	data[i] = data[i].get_text() # Cleaning relevant playlist information
-data[3] = img
-data.append(title)
-del header
+# TODO: done up to this point
 
 # Videos div
 soup = soup.find(id='pl-load-more-destination')
